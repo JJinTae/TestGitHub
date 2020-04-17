b3e1ff248c06af0e67c82060e9617a0e07f226fb
jjintae <emflaxl100@naver.com>
2020-04-18 00:43:43 +0900

    Init testbranch2 - gen new activity

--- app/src/main/java/com/example/testgithub/Main2Activity.java
+++ app/src/main/java/com/example/testgithub/Main2Activity.java
@@ -3,15 +3,15 @@ package com.example.testgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
-    TextView text;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
