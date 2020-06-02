a33e1e8afb31e8e01e9908fdb66a21bb94070ccf
kimyeonah <yeny28@daum.net>
2020-06-02 18:15:59 +0900

    YA test

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.testgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
