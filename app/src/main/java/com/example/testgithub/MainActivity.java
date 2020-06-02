7e0701a635caf9c5ca12f8a6b454babcd4244f05
JJinTae <emflaxl100@naver.com>
2020-06-02 18:13:47 +0900

    JT Test

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    TextView test1; // JT
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test1 = (TextView)findViewById(R.id.text1);
     }
+
+
 }
